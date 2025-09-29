ee91ed324d8687ff3fb514d2c94018acc8c03e7e
Belinszky Tamás <31423611+tamasbelinszky@users.noreply.github.com>
2025-09-29 18:38:32 +0200

    Configure Coolify's magic envs and local overrides (#4)

--- app/hatchet_client.py
+++ app/hatchet_client.py
@@ -5,6 +5,5 @@ from app.config import settings
 hatchet = Hatchet(
     config=ClientConfig(
         token=settings.HATCHET_CLIENT_TOKEN,
-        host_port=settings.HATCHET_CLIENT_HOST_PORT,
     )
 )
